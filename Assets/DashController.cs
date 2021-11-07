1e8c0048ae48a803fbe45c5a09ef639c6b9b8737
Ivan <ivan.sales@enti.cat>
2021-11-07 20:26:53 +0100

    Primer commit dash

---
+++ Assets/DashController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    /*public float dashSpeed;
+    private float timer;
+    public float dashDistance;
+    private bool isDashing=false;
+    public GameObject targetDash;*/
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //timer = dashDistance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (!isDashing) {
+            if (Input.GetButtonDown("R2") || Input.GetButtonDown("L2"))
+            {
+                isDashing = true;
+            }
+        }
+        else
+        {
+            if (timer <= 0)
+            {
+                isDashing=false;
+                timer = dashDistance;
+                rb.velocity = Vector2.zero;
+            } else
+            {
+                timer -= Time.deltaTime;
+                rb.velocity = targetDash.transform.position * dashSpeed;
+            }
+        }*/
+
+        if (Input.GetButtonDown("R2") || Input.GetButtonDown("L2"))
+        {
+            //isDashing = true;
+            this.transform.position += this.GetComponent<movement>().lastMoveDir * 5f;
+            //rb.velocity += this.GetComponent<movement>().lastMoveDir * 5f;
+        }
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed=15.0f;
+
+    public float rotationSpeed=1000.0f;
+
+    public Vector3 lastMoveDir;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+        lastMoveDir = movementDirection;
+        transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
