6ac96c3abbc73a53d48cefb30fb6f651a8e4a4e4
Ivan Sales <ivan.sales@enti.cat>
2021-11-08 16:00:30 +0100

    Enemigo basico bastante done

--- Assets/Proyecto/Scripts/HealthBarController.cs
+++ Assets/Proyecto/Scripts/HealthBarController.cs
@@ -8,24 +8,51 @@ public class HealthBarController : MonoBehaviour
     public Slider healthBar;
     public Vector3 offSet;
     public float showTime;
+    private float currentTimer;
+    private float maxTimer;
+    private bool fadeOut=false;
+    public float fadeOutSpeed;
+
+    public void FadeOutHealthBar()
+    {
+        float  fadeAmount = this.GetComponent<CanvasGroup>().alpha - (fadeOutSpeed * Time.deltaTime);
+
+        this.GetComponent<CanvasGroup>().alpha = fadeAmount;
+
+        if (this.GetComponent<CanvasGroup>().alpha <= 0f)
+        {
+            fadeOut = false;
+        }
+    }
 
     public void SetHealthBar(float currentHealth, float maxHealth)
-    {
+    { 
         healthBar.maxValue = maxHealth;
         if (currentHealth < maxHealth)
         {
             healthBar.gameObject.SetActive(true);
-        }
-        else
-        {
-            healthBar.gameObject.SetActive(false);
+            this.GetComponent<CanvasGroup>().alpha = 1;
         }
         healthBar.value = currentHealth;
+        maxTimer = currentTimer + showTime;
+        fadeOut = true;
+    }
+
+    private void Start()
+    {
+        //maxTimer = Time.deltaTime + showTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         healthBar.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + offSet);
+
+        currentTimer += Time.deltaTime;
+
+        if (currentTimer > maxTimer && fadeOut)
+        {
+            FadeOutHealthBar();
+        }
     }
 }
