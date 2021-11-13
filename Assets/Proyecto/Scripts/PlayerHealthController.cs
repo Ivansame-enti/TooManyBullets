986850521f6b6d864ea3f4a514c874fec4c11a46
Ivan <ivan.sales@enti.cat>
2021-11-14 00:09:03 +0100

    Faltan sonidos y comprobar parametros

--- Assets/Proyecto/Scripts/PlayerHealthController.cs
+++ Assets/Proyecto/Scripts/PlayerHealthController.cs
@@ -8,19 +8,51 @@ public class PlayerHealthController : MonoBehaviour
     public int currentHealth;
     public Animator shakeCamera;
     public float inmortalTime;
+    public float blinkTime;
     private float timer;
+    private float timer2;
+    private float fixedDeltaTime;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = health;
-        timer = inmortalTime;
+        //timer = inmortalTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer > 0) timer -= Time.deltaTime;
+        //Debug.Log(Time.timeScale);
+
+        if (timer > 0) //Tiempo donde es imnnune porque le han golpeado
+        {
+            if(Time.timeScale < 1.0f)
+            {
+                Time.timeScale += Time.deltaTime;
+            }
+            
+            if(timer2 <= 0) // Timer que controla el parpadeo que inndica la inmortalidad
+            {
+                if (this.GetComponent<SpriteRenderer>().color.a == 0f)
+                {
+                    this.GetComponent<SpriteRenderer>().color = new Color(this.GetComponent<SpriteRenderer>().color.r, this.GetComponent<SpriteRenderer>().color.g, this.GetComponent<SpriteRenderer>().color.b, 1f);
+                } else
+                {
+                    this.GetComponent<SpriteRenderer>().color = new Color(this.GetComponent<SpriteRenderer>().color.r, this.GetComponent<SpriteRenderer>().color.g, this.GetComponent<SpriteRenderer>().color.b, 0f);
+                }
+                timer2 = blinkTime;
+            } else
+            {
+                timer2 -= Time.deltaTime;
+            }
+
+            timer -= Time.deltaTime;
+        } else //Vuelve a la normalidad
+        {
+            Time.timeScale = 1.0f;
+            this.GetComponent<SpriteRenderer>().color = new Color(this.GetComponent<SpriteRenderer>().color.r, this.GetComponent<SpriteRenderer>().color.g, this.GetComponent<SpriteRenderer>().color.b, 1f);
+        }
 
         if(currentHealth <= 0)
         {
@@ -31,11 +63,12 @@ public class PlayerHealthController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        currentHealth--;
-        shakeCamera.SetTrigger("Shake");
-        timer = inmortalTime;
-
-        //shakeCamera.SetTrigger("Shake");
-        //Debug.Log("Damage");
+        if (timer <= 0)
+        {
+            currentHealth--;
+            shakeCamera.SetTrigger("Shake");
+            timer = inmortalTime;
+            Time.timeScale = 0.2f;
+        }
     }
 }
