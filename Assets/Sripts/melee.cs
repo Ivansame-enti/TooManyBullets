9b3a658e8a4398edfc474ed24f499220d4fa9434
Ivan <ivan.sales@enti.cat>
2021-11-16 23:29:48 +0100

    Mejoras en ataque melee

--- Assets/Sripts/melee.cs
+++ Assets/Sripts/melee.cs
@@ -8,7 +8,7 @@ public class melee : MonoBehaviour
     public GameObject ataque;
     public GameObject player;
     private bool ataquet = false;
-    public float timer=1f;
+    public float timer = 1f;
     private GameObject ataquei;
     private GameObject ataquei2;
     private GameObject ataquei3;
@@ -16,17 +16,18 @@ public class melee : MonoBehaviour
     public float JoystickXRange;
     public float JoystickYRange;
     float xPos, yPos;
-    float result, result2;
+    float result, result2, result3, result4;
+    private float sensivity = 0.3f;
 
-    private void Damage(float rotx, float roty)
+    private void Damage(float rotx, float roty, float rotx2, float roty2)
     {
         ataquei2 = Instantiate(ataque);
-            ataquei2.SetActive(true);
-            ataquei2.transform.position = new Vector2(transform.position.x + xPos * 2, transform.position.y + yPos * -2);
-            //ataquei2.transform.rotation = Quaternion.Slerp(ataquei2.transform.rotation, Quaternion.LookRotation(new Vector3(xPos, yPos, 0)), Time.deltaTime * 5f);
-            ataquei2.transform.rotation = Quaternion.Euler(new Vector3(0,0, roty-rotx));
+        //ataquei2.SetActive(true);
+        ataquei2.transform.position = new Vector2(transform.position.x + xPos * 2, transform.position.y + yPos * -2);
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, roty2 - rotx2));
+        ataquei2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, roty - rotx));
 
-        //Destroy(this.ataquei2, 1);
+        Destroy(this.ataquei2, 1);
         timer = 1f;
     }
 
@@ -39,68 +40,6 @@ public class melee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
-        /*if (Input.GetKeyDown("w"))
-        {
-            Timer += Time.deltaTime;
-            print("w was pressed");
-           // Instantiate(ataque);
-            ataquei = Instantiate(ataque);
-            ataquet = true;
-            if (ataquet == true)
-            {
-                ataquei.SetActive(true);
-                ataquei.transform.position = new Vector2(this.GetComponent<movement>().lastMoveDir.x + transform.position.x, this.GetComponent<movement>().lastMoveDir.y + 2 + transform.position.y);
-                ataquei.transform.rotation = Quaternion.Slerp(ataquei.transform.rotation, Quaternion.LookRotation(this.GetComponent<movement>().lastMoveDir), Time.deltaTime * 40f);
-            }
-          
-                Destroy(this.ataquei,1);
-        }
-        if (Input.GetKeyDown("a"))
-        {
-            print("a was pressed");
-          //  Instantiate(ataque);
-            ataquei2 = Instantiate(ataque);
-            ataquet = true;
-            if (ataquet == true)
-            {
-                ataquei2.SetActive(true);
-                ataquei2.transform.position = new Vector2(this.GetComponent<movement>().lastMoveDir.x - 2 + transform.position.x, this.GetComponent<movement>().lastMoveDir.y + 0 + transform.position.y);
-                ataquei2.transform.rotation = Quaternion.Slerp(ataquei.transform.rotation, Quaternion.LookRotation(this.GetComponent<movement>().lastMoveDir), Time.deltaTime * 40f);
-            }
-            Destroy(this.ataquei2, 1);
-        }
-        if (Input.GetKeyDown("d"))
-        {
-            print("d was pressed");
-          //  Instantiate(ataque);
-            ataquei3 = Instantiate(ataque);
-            ataquet = true;
-            if (ataquet == true)
-            {
-                ataquei3.SetActive(true);
-                ataquei3.transform.position = new Vector2(this.GetComponent<movement>().lastMoveDir.x + 2 + transform.position.x, this.GetComponent<movement>().lastMoveDir.y +0+ transform.position.y);
-                ataquei3.transform.rotation = Quaternion.Slerp(ataquei.transform.rotation, Quaternion.LookRotation(this.GetComponent<movement>().lastMoveDir), Time.deltaTime * 40f);
-            }
-            Destroy(this.ataquei3, 1);
-        }
-        if (Input.GetKeyDown("s"))
-        {
-            print("s was pressed");
-         //   Instantiate(ataque);
-            ataquei4 = Instantiate(ataque);
-            ataquet = true;
-            if (ataquet == true)
-            {
-                ataquei4.SetActive(true);
-                ataquei4.transform.position = new Vector2(this.GetComponent<movement>().lastMoveDir.x + 0 + transform.position.x, this.GetComponent<movement>().lastMoveDir.y - 2 + transform.position.y);
-                ataquei4.transform.rotation = Quaternion.Slerp(ataquei.transform.rotation, Quaternion.LookRotation(this.GetComponent<movement>().lastMoveDir), Time.deltaTime * 40f);
-            }
-            Destroy(this.ataquei4, 1);
-        }
-    }*/
-        
         if (timer <= 0)
         {
             xPos = Input.GetAxis("RightJoystickX");
@@ -108,21 +47,17 @@ public class melee : MonoBehaviour
 
             if ((xPos >= JoystickXRange || xPos <= -JoystickXRange) || (yPos >= JoystickYRange || yPos <= -JoystickYRange))
             {
-                Debug.Log("Y" + Input.GetAxis("RightJoystickY"));
-                Debug.Log("X" + Input.GetAxis("RightJoystickX"));
-
-                //float z = (xPos - (1)) / (-1 - (+1)) * (0 - 180) + 180;
-
-                //float z2 = (yPos - (-1)) / (1 - (-1)) * (270 - 90) + 90;
                 if ((xPos >= JoystickXRange || xPos <= -JoystickXRange)) result = Mathf.Lerp(0, 180, Mathf.InverseLerp(1, -1, xPos));
                 else result = 0;
                 if ((yPos >= JoystickYRange || yPos <= -JoystickYRange)) result2 = Mathf.Lerp(90, 270, Mathf.InverseLerp(-1, 1, yPos));
                 else result2 = 0;
 
-                Debug.Log(result);
-                Debug.Log("2: " + result2);
-                //Vector3 rotacion = new Vector3(0,0,0);
-                Damage(result, result2);
+                if ((xPos >= JoystickXRange || xPos <= -JoystickXRange)) result3 = Mathf.Lerp(90, 270, Mathf.InverseLerp(1, -1, xPos));
+                else result3 = 0;
+                if ((yPos >= JoystickYRange || yPos <= -JoystickYRange)) result4 = Mathf.Lerp(0, 180, Mathf.InverseLerp(-1, 1, yPos));
+                else result4 = 0;
+
+                Damage(result, result2, result3, result4);
             }
         }
         else
