cfc02baabc78d5838106aa06a55ee81be5007dc4
Pablo <pperpinyanc@gmail.com>
2021-11-17 01:39:50 +0100

    Ataque a melle con particulas(falla destruir)

--- Assets/Sripts/melee.cs
+++ Assets/Sripts/melee.cs
@@ -18,10 +18,16 @@ public class melee : MonoBehaviour
     float xPos, yPos;
     float result, result2, result3, result4;
     private float sensivity = 0.3f;
+    public ParticleSystem particles;
+    private ParticleSystem particles2;
 
     private void Damage(float rotx, float roty, float rotx2, float roty2)
     {
         ataquei2 = Instantiate(ataque);
+      //  particles2 = Instantiate(particles);
+
+        Instantiate(particles, new Vector2(transform.position.x + xPos * 2, transform.position.y + yPos * -2), Quaternion.identity);
+        // Destroy(particles2, 1);
         //ataquei2.SetActive(true);
         ataquei2.transform.position = new Vector2(transform.position.x + xPos * 2, transform.position.y + yPos * -2);
         this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, roty2 - rotx2));
@@ -31,6 +37,8 @@ public class melee : MonoBehaviour
         timer = 1f;
     }
 
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -58,6 +66,72 @@ public class melee : MonoBehaviour
                 else result4 = 0;
 
                 Damage(result, result2, result3, result4);
+            }
+            else if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                ataquei2 = Instantiate(ataque);
+                //Instantiate(particles);
+                //  particles2 = Instantiate(particles);
+
+                //  particles2.transform.position = new Vector2(transform.position.x + -1 * 2, transform.position.y + 0 * -2);
+                // particles2.Play();
+                Instantiate(particles, new Vector2(transform.position.x + -1 * 2, transform.position.y + 0 * -2), Quaternion.identity);
+                // Destroy(particles2, 1);
+
+                //ataquei2.SetActive(true);
+                ataquei2.transform.position = new Vector2(transform.position.x + -1 * 2, transform.position.y + 0 * -2);
+                this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90 - 0));
+                ataquei2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180 - 0));
+
+                Destroy(this.ataquei2, 1);
+                timer = 1f;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                ataquei2 = Instantiate(ataque);
+                //ataquei2.SetActive(true);
+                // particles2 = Instantiate(particles);
+
+                //  particles2.transform.position = new Vector2(transform.position.x + +1 * 2, transform.position.y + 0 * -2);
+                //  particles2.Play();
+                // Destroy(particles2, 1);
+                Instantiate(particles, new Vector2(transform.position.x + +1 * 2, transform.position.y + 0 * -2), Quaternion.identity);
+                ataquei2.transform.position = new Vector2(transform.position.x + +1 * 2, transform.position.y + 0 * -2);
+                this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270 - 0));
+                ataquei2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0 - 0));
+
+                Destroy(this.ataquei2, 1);
+                timer = 1f;
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                ataquei2 = Instantiate(ataque);
+                //ataquei2.SetActive(true);
+                // particles2 = Instantiate(particles);
+
+                // particles2.transform.position = new Vector2(transform.position.x + 0 * 2, transform.position.y + -1 * -2);
+                //  particles2.Play();
+                //Destroy(particles2, 1);
+                Instantiate(particles, new Vector2(transform.position.x + 0 * 2, transform.position.y + -1 * -2), Quaternion.identity);
+                ataquei2.transform.position = new Vector2(transform.position.x + 0 * 2, transform.position.y + -1 * -2);
+                this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0 - 0));
+                ataquei2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0 + 90));
+
+                Destroy(this.ataquei2, 1);
+                timer = 1f;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                ataquei2 = Instantiate(ataque);
+                //ataquei2.SetActive(true);
+                Instantiate(particles, new Vector2(transform.position.x + 0 * 2, transform.position.y + 1 * -2), Quaternion.identity);
+                // Destroy(particles2, 1);
+                ataquei2.transform.position = new Vector2(transform.position.x + 0 * 2, transform.position.y + 1 * -2);
+                this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0 - 180));
+                ataquei2.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0 - 90));
+
+                Destroy(this.ataquei2, 1);
+                timer = 1f;
             }
         }
         else
