9ae84a28ec1aa2dfc343216bca3bfcd0031bed85
Josep <josep.romera@enti.cat>
2021-11-17 01:45:05 +0100

    Tutorial hecho

---
+++ Assets/Proyecto/Scripts/Enemy1/TutorialEnemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialEnemy : MonoBehaviour
+{
+    public float health;
+    private float maxHealth;
+    public GameObject hitPS;
+    public GameObject deathPS;
+    public GameObject swPs;
+    public HealthBarController healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("nivel comienza!");
+            Destroy(this.gameObject);
+            Instantiate(deathPS, this.transform.position, Quaternion.identity);
+            Instantiate(swPs, this.transform.position, Quaternion.identity);
+            FindObjectOfType<AudioManagerController>().AudioPlay("Enemy1Death");
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(hitPS, new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Quaternion.identity);
+            health--;
+            healthBar.SetHealthBar(health, maxHealth);
+            if (health > 0) FindObjectOfType<AudioManagerController>().AudioPlay("Enemy1Hit");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //Instantiate(hitPS, new Vector2(this.transform.position.x, this.transform.position.y - 0.5f), Quaternion.identity);
+        //health = 0;
+    }
+}
--- Assets/Proyecto/Scripts/Nivel1/Tutorial.cs
+++ Assets/Proyecto/Scripts/Nivel1/Tutorial.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class Tutorial : MonoBehaviour
 {
     public GameObject tutorialNPC1, celestialAtk;
+    private int flag = 0;
+    //Laser 
     Transform m_transform;
     Vector2 laserPos1, laserPos2;
     public LineRenderer m_lineRenderer;
     public GameObject collisionLaser;
     public float defDistanceRay = 100;
     public GameObject laserParticles;
+    //Enemigo prueba
+    public GameObject tutorialEnemy;
+    public GameObject tutorialImageControls,tutorialImageDash,tutorialImageAttack;
 
     //private new Vector2();
     // Start is called before the first frame update
     void Start()
     {
+        tutorialImageControls.SetActive(true);
+        tutorialImageDash.SetActive(false);
+        tutorialImageAttack.SetActive(false);
         m_transform = GetComponent<Transform>();
-
         laserPos1 = new Vector2(0,30);
         laserPos2 = new Vector2(0,-100000000);
-
+        tutorialEnemy.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,9 +38,21 @@ public class Tutorial : MonoBehaviour
 
     private void tutorial()
     {
-        this.transform.position = new Vector2(-12, 0);
-        celestialAtk.SetActive(true);
-        Debug.Log("ola");
+        if(flag == 0)
+        {
+            tutorialImageControls.SetActive(false);
+            tutorialImageDash.SetActive(true);
+            this.transform.position = new Vector2(-12, 0);
+            celestialAtk.SetActive(true);
+        }
+        if(flag == 1)
+        {
+           tutorialImageDash.SetActive(false);
+           tutorialImageAttack.SetActive(true);
+           celestialAtk.SetActive(false);
+           tutorialEnemy.SetActive(true);
+           this.transform.position = new Vector2(-30, 0);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -41,18 +60,15 @@ public class Tutorial : MonoBehaviour
         if (collision.gameObject.tag.Equals("PlayerTag"))
         {
             tutorial();
+            flag++;
         }
     }
     void ShootLaser()
     {
-
         Draw2DRay(laserPos1, laserPos2 * defDistanceRay);
         collisionLaser.transform.position = laserPos1;
         laserParticles.transform.position = laserPos1;
-        //laserParticles.SetActive(true);
-
-
-        //}
+        laserParticles.SetActive(true);
     }
 
     void Draw2DRay(Vector2 startPos, Vector2 endPos)
