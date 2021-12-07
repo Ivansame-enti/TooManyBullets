4d4b6584be29eab521a857079f667e4c5b60ac98
Ivan <ivan.sales@enti.cat>
2021-12-07 21:05:41 +0100

    Intento de que las balas cambien su tamaño al usar escudo

--- Assets/Proyecto/Scripts/Enemy1/Enemy1BulletController.cs
+++ Assets/Proyecto/Scripts/Enemy1/Enemy1BulletController.cs
@@ -33,7 +33,18 @@ public class Enemy1BulletController : MonoBehaviour
 
         if (collision.gameObject.tag.Equals("DestroyBulletCollider"))
         {
+            //StartCoroutine(ScaleOverTime(1, collision));
+            //collision.gameObject.transform.localScale = new
             Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag.Equals("Shield"))
+        {
+            //Debug.Log("a");
+            //StartCoroutine(ScaleOverTime(1, collision));
+            collision.gameObject.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
+            //collision.gameObject.transform.localScale = new
+            //Destroy(gameObject);
         }
     }
 
@@ -41,5 +52,23 @@ public class Enemy1BulletController : MonoBehaviour
     {
         //Destroy(gameObject);
         Debug.Log("colision");
+    }
+
+    IEnumerator ScaleOverTime(float time, Collider2D collision)
+    {
+        Vector3 originalScale = collision.transform.localScale;
+        Vector3 destinationScale = new Vector3(0.0f, 0.0f, 0.0f);
+        float currentTime = 0.0f;
+
+        do
+        {
+            //originalScale.x -= Time.deltaTime;
+            //originalScale.y -= Time.deltaTime;
+            collision.transform.localScale = destinationScale;
+            currentTime += Time.deltaTime;
+            yield return null;
+        } while (currentTime <= time);
+
+        Destroy(collision.gameObject);
     }
 }
