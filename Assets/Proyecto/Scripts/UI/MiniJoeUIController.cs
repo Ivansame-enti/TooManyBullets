32ff95f32d2e28c92acf61352112be721f7eb84d
Ivan <ivan.sales@enti.cat>
2021-12-08 23:40:30 +0100

    Acabado?

--- Assets/Proyecto/Scripts/UI/MiniJoeUIController.cs
+++ Assets/Proyecto/Scripts/UI/MiniJoeUIController.cs
@@ -5,48 +5,106 @@ using UnityEngine.UI;
 
 public class MiniJoeUIController : MonoBehaviour
 {
-    public GameObject plantImage;
+    public GameObject plant;
+    public Image plantImage;
     public Image plantImageGreen;
-    public Image activeImage1;
-    public Image activeImage2;
+
+    public GameObject activeIn;
+    public Image activeInImage1;
+    public Image activeInImage2;
+    public Image activeInImageGreen;
+
+    public GameObject pasive;
+    public Image pasiveImage;
+    public Image pasiveImageGreen;
+
+    public GameObject activeOut;
+    public Image activeOutImage1;
+    public Image activeOutImage2;
+    public Image activeOutImage3;
+    public Image activeOutImageGreen;
+
     public MiniJoeHealController mhc;
+
     private Color firstColorPlant;
     private Color firstColorActive1;
     private Color firstColorActive2;
+    private Color firstColorPasive;
+    private Color firstColorOutActive1;
+    private Color firstColorOutActive2;
+    private Color firstColorOutActive3;
+
     public bool active;
+    public bool miniJoeIn;
 
     // Start is called before the first frame update
     void Start()
     {
-        firstColorPlant = plantImage.GetComponent<Image>().color;
-        firstColorActive1 = activeImage1.color;
-        firstColorActive2 = activeImage2.color;
+        plant.SetActive(true);
+        firstColorPlant = plantImage.color;
+        firstColorActive1 = activeInImage1.color;
+        firstColorActive2 = activeInImage2.color;
+        firstColorPasive = pasiveImage.color;
+        firstColorOutActive1 = activeOutImage1.color;
+        firstColorOutActive2 = activeOutImage2.color;
+        firstColorOutActive3 = activeOutImage3.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (mhc.timer2 >= mhc.healDelay)
+        if (miniJoeIn) //Cuando miniJoe va contigo
         {
-            plantImage.gameObject.GetComponent<Animator>().enabled = true;
-        } else
-        {
-            plantImage.gameObject.GetComponent<Animator>().enabled = false;
-            plantImage.GetComponent<Image>().color = firstColorPlant;
-        }
+            activeIn.SetActive(true);
+            pasive.SetActive(true);
+            activeOut.SetActive(false);
+        
+            if (mhc.timer2 >= mhc.healDelay) //Curacion
+            {
+                //plantImage.gameObject.GetComponent<Animator>().enabled = true;
+                pasiveImage.gameObject.GetComponent<Animator>().enabled = true;
+            }
+            else
+            {
+                pasiveImage.gameObject.GetComponent<Animator>().enabled = false;
+                pasiveImage.color = firstColorPasive;
+            }
 
-        plantImageGreen.fillAmount = mhc.timer2/mhc.healDelay;
+            pasiveImageGreen.fillAmount = mhc.timer2 / mhc.healDelay;
 
-        if (active)
+            if (active) //Escudo
+            {
+                activeInImage1.gameObject.GetComponent<Animator>().enabled = true;
+                activeInImage2.gameObject.GetComponent<Animator>().enabled = true;
+            }
+            else
+            {
+                activeInImage1.gameObject.GetComponent<Animator>().enabled = false;
+                activeInImage2.gameObject.GetComponent<Animator>().enabled = false;
+                activeInImage1.color = firstColorActive1;
+                activeInImage2.color = firstColorActive2;
+            }
+        } else //Minioe plantado
         {
-            activeImage1.gameObject.GetComponent<Animator>().enabled = true;
-            activeImage2.gameObject.GetComponent<Animator>().enabled = true;
-        } else
-        {
-            activeImage1.gameObject.GetComponent<Animator>().enabled = false;
-            activeImage2.gameObject.GetComponent<Animator>().enabled = false;
-            activeImage1.color = firstColorActive1;
-            activeImage2.color = firstColorActive2;
+            activeIn.SetActive(false);
+            pasive.SetActive(false);
+            activeOut.SetActive(true);
+
+            if (active) //Rayo
+            {
+                activeOutImage1.gameObject.GetComponent<Animator>().enabled = true;
+                activeOutImage2.gameObject.GetComponent<Animator>().enabled = true;
+                activeOutImage3.gameObject.GetComponent<Animator>().enabled = true;
+            }
+            else
+            {
+                activeOutImage1.gameObject.GetComponent<Animator>().enabled = false;
+                activeOutImage2.gameObject.GetComponent<Animator>().enabled = false;
+                activeOutImage3.gameObject.GetComponent<Animator>().enabled = false;
+                activeOutImage1.color = firstColorOutActive1;
+                activeOutImage2.color = firstColorOutActive2;
+                activeOutImage3.color = firstColorOutActive3;
+            }
         }
     }
 }
