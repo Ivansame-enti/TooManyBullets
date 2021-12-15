29ae32e35ab2a918099d6dd7857e8643071f9ff1
Josep <josep.romera@enti.cat>
2021-12-16 00:31:40 +0100

    Parte del nivel completado

--- Assets/Proyecto/Scripts/Nivel1/Nivel1.cs
+++ Assets/Proyecto/Scripts/Nivel1/Nivel1.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class Nivel1 : MonoBehaviour
 {
-    public bool startLevel;
-    public GameObject enemies,scenarioAttacks;
+    public bool startLevel, clearPart1;
+    public GameObject part1, part2, scenarioAttacks;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
         startLevel = false;
+        clearPart1 = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (startLevel == true)
         {
             FindObjectOfType<Tutorial>().tutorialImageAttack.SetActive(false);
-            enemies.SetActive(true);
+            part1.SetActive(true);
             scenarioAttacks.SetActive(true);
+        }
+
+        if (part1.transform.childCount <= 0)
+        {
+            part2.SetActive(true);
+        }
+
+        if (part2.transform.childCount <= 0 && clearPart1 == false)
+        {
+            timer = 3f;
+            clearPart1 = true;
+
+
+        }
+        if (part2.transform.childCount <= 0 && timer < 0)
+        {
+            Debug.Log(timer);
+        }
+        else
+        {
+            timer -= Time.deltaTime;
         }
     }
 }
