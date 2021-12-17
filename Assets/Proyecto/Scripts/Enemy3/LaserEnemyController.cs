9688a79749c0abb8b6a5f47eb1392f1401d87bba
Ivan Sales <ivan.sales@enti.cat>
2021-12-17 17:11:03 +0100

    Intento de hacer que el laser choque con el jugador

--- Assets/Proyecto/Scripts/Enemy3/LaserEnemyController.cs
+++ Assets/Proyecto/Scripts/Enemy3/LaserEnemyController.cs
@@ -29,7 +29,7 @@ public class LaserEnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         transform.Rotate(0, 0, speedRotation * Time.deltaTime); //Rotacion
         exclamation1.transform.rotation = originalRotation;
         exclamation2.transform.rotation = originalRotation;
@@ -64,21 +64,23 @@ public class LaserEnemyController : MonoBehaviour
             mLaserBeam3.GetComponent<LineRenderer>().SetColors(new Color(255, 0, 146), new Color(255, 0, 146));
             mLaserBeam3.GetComponent<LineRenderer>().SetWidth(0.30f, 0.30f);
 
-            if(timer2 >= attackDuration)
+            if (timer2 >= attackDuration)
             {
                 timer = 0;
                 timer2 = 0;
                 mLaserBeam.SetActive(false);
                 mLaserBeam2.SetActive(false);
                 mLaserBeam3.SetActive(false);
-            } else
+            }
+            else
             {
                 timer2 += Time.deltaTime;
             }
 
-        } else
+        }
+        else
         {
-            if(timer>= fireRate - warningTime)
+            if (timer >= fireRate - warningTime)
             {
                 warning = true;
                 ShootLaser();
@@ -120,8 +122,11 @@ public class LaserEnemyController : MonoBehaviour
                 col = new GameObject("Collider").AddComponent<BoxCollider2D>();
                 col.tag = "LaserColliderEnemy";
                 col.gameObject.layer = 8;
-                col.transform.parent = mLaserBeam.transform; // Collider is added as child object of line
+                //col.gameObject.AddComponent<Rigidbody2D>();
+                col.gameObject.AddComponent<LaserCollisionController>();
 
+                col.isTrigger = true;
+                col.transform.parent = mLaserBeam.transform; // Collider is added as child object of line
                 float lineLength = Vector3.Distance(startPos, endPos); // length of line
 
                 col.size = new Vector3(lineLength, 0.3f, 1f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
@@ -137,7 +142,6 @@ public class LaserEnemyController : MonoBehaviour
                 }
                 angle = Mathf.Rad2Deg * Mathf.Atan(angle);
                 col.transform.Rotate(0, 0, angle);
-                col.isTrigger = true;
             }
         }
 
@@ -152,8 +156,10 @@ public class LaserEnemyController : MonoBehaviour
                 col2 = new GameObject("Collider2").AddComponent<BoxCollider2D>();
                 col2.tag = "LaserColliderEnemy";
                 col2.gameObject.layer = 8;
+                //col2.gameObject.AddComponent<Rigidbody2D>();
+                col2.gameObject.AddComponent<LaserCollisionController>();
+                col2.isTrigger = true;
                 col2.transform.parent = mLaserBeam2.transform; // Collider is added as child object of line
-
                 float lineLength = Vector3.Distance(startPos, endPos); // length of line
 
                 col2.size = new Vector3(lineLength, 0.3f, 1f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
@@ -169,7 +175,6 @@ public class LaserEnemyController : MonoBehaviour
                 }
                 angle = Mathf.Rad2Deg * Mathf.Atan(angle);
                 col2.transform.Rotate(0, 0, angle);
-                col2.isTrigger = true;
             }
         }
 
@@ -181,11 +186,14 @@ public class LaserEnemyController : MonoBehaviour
             if (col3 != null) Destroy(col3.gameObject);
             if (!warning)
             {
+
                 col3 = new GameObject("Collider3").AddComponent<BoxCollider2D>();
                 col3.tag = "LaserColliderEnemy";
                 col3.gameObject.layer = 8;
+                //col3.gameObject.AddComponent<Rigidbody2D>();
+                col3.gameObject.AddComponent<LaserCollisionController>();
                 col3.transform.parent = mLaserBeam3.transform; // Collider is added as child object of line
-
+                col3.isTrigger = true;
                 float lineLength = Vector3.Distance(startPos, endPos); // length of line
 
                 col3.size = new Vector3(lineLength, 0.3f, 1f); // size of collider is set where X is length of line, Y is width of line, Z will be set as per requirement
@@ -201,7 +209,6 @@ public class LaserEnemyController : MonoBehaviour
                 }
                 angle = Mathf.Rad2Deg * Mathf.Atan(angle);
                 col3.transform.Rotate(0, 0, angle);
-                col3.isTrigger = true;
             }
         }
     }
