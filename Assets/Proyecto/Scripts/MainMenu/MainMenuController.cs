77f0dae2d18aa9d88ad0dbf8accd8eeca2a0b8d5
Josep <josep.romera@enti.cat>
2022-01-03 19:33:27 +0100

    Menu,opciones y selector de niveles hecho

--- Assets/Proyecto/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Proyecto/Scripts/MainMenu/MainMenuController.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.EventSystems;
 public class MainMenuController : MonoBehaviour
 {
+    public GameObject playButton, optionButton, exitButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,15 +21,23 @@ public class MainMenuController : MonoBehaviour
     public void MainMenu()
     {
         SceneManager.LoadScene("LevelSelector");
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(playButton);
     }
 
     public void Options()
     {
         SceneManager.LoadScene("Options");
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionButton);
     }
 
     public void QuitGame()
     {
         Application.Quit();
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(exitButton);
     }
 }
