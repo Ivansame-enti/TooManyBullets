99f66d5e59f6fd405e0c2a8d74e8b9410c904081
Ivan <ivan.sales@enti.cat>
2022-01-07 00:58:43 +0100

    Nivel hecho, hay que probarlo

--- Assets/Level3Controller.cs
+++ Assets/Level3Controller.cs
@@ -6,19 +6,52 @@ public class Level3Controller : MonoBehaviour
 {
     public GameObject phase1;
     public GameObject phase2;
+    public GameObject phase3;
+    public GameObject phase4;
+    public GameObject scenario;
     private int phasecounter;
+    private bool oneTime;
     // Start is called before the first frame update
     void Start()
     {
         phasecounter = 0;
         phase1.SetActive(true);
         phase2.SetActive(false);
+        phase3.SetActive(false);
+        phase4.SetActive(false);
+        scenario.SetActive(false);
+        oneTime = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (phase2.transform.childCount <= 0 && phasecounter == 2)
+        {
+            phase2.SetActive(false);
+            phase3.SetActive(true);
+            phasecounter++;
+        }
+
+        if (phase3.transform.childCount <= 0 && phasecounter == 3)
+        {
+            if (oneTime)
+            {
+                GameObject[] bullets;
+
+                bullets = GameObject.FindGameObjectsWithTag("EnemyBullet");
+
+                foreach (GameObject bullet in bullets)
+                {
+                    Destroy(bullet);
+                }
+                oneTime = false;
+            }
+            scenario.SetActive(false);
+            phase3.SetActive(false);
+            phase4.SetActive(true);
+            //phasecounter++;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -34,6 +67,7 @@ public class Level3Controller : MonoBehaviour
             {
                 phase1.SetActive(false);
                 phase2.SetActive(true);
+                scenario.SetActive(true);
                 GameObject[] bullets;
 
                 bullets = GameObject.FindGameObjectsWithTag("EnemyBullet");
@@ -43,6 +77,7 @@ public class Level3Controller : MonoBehaviour
                     Destroy(bullet);
                 }
                 phasecounter++;
+                this.transform.position = new Vector3(0f, 50f, 1);
             }
         }
     }
