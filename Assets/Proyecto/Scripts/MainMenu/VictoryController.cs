3c3a1bcd45b76a37f643b88e1b399f7e80aec06e
Josep <josep.romera@enti.cat>
2022-01-08 00:11:36 +0100

    Petardos casi hechos

--- Assets/Proyecto/Scripts/MainMenu/VictoryController.cs
+++ Assets/Proyecto/Scripts/MainMenu/VictoryController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class VictoryController : MonoBehaviour
 {
+    private Vector2 randomValor,randomValor2;
+    public Vector2 positionA, positionB,positionC,positionD;
+    public GameObject firework;
+    private GameObject fireworkClone,fireworkClone2;
+    private float leftFirework,rightFirework;
+    public float fireworkCooldownLeft, fireworkCooldownRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (leftFirework <= 0)
+        {
+            randomValor = new Vector2(
+                Random.Range(positionA.x, positionB.x),
+                Random.Range(positionA.y, positionB.y)
+        );
+            Instantiate(firework, randomValor, Quaternion.identity);
+            leftFirework = fireworkCooldownLeft;
+        }
+        else
+        {
+            leftFirework -= Time.deltaTime;
+        }
+
+        if (rightFirework <= 0)
+        {
+            randomValor2 = new Vector2(
+                Random.Range(positionC.x, positionD.x),
+                Random.Range(positionC.y, positionD.y)
+             );
+            Instantiate(firework, randomValor2, Quaternion.identity);
+            rightFirework = fireworkCooldownRight;
+        }
+        else
+        {
+            rightFirework -= Time.deltaTime;
+        }
     }
 }
