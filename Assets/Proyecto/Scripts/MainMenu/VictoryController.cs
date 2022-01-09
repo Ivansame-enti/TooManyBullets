d1a6d4b8ac5130be67b61867f09115d07b588422
Ivan <ivan.sales@enti.cat>
2022-01-10 00:54:52 +0100

    Arreglado problema con los petardos de victoria

--- Assets/Proyecto/Scripts/MainMenu/VictoryController.cs
+++ Assets/Proyecto/Scripts/MainMenu/VictoryController.cs
@@ -7,17 +7,18 @@ public class VictoryController : MonoBehaviour
 {
     private Vector2 randomValor, randomValor2;
     public Vector2 positionA, positionB, positionC, positionD;
-    public GameObject firework, victoryUI, player, victoryPanel,buttonLs,levelObjects;
+    public GameObject firework, victoryUI, player, victoryPanel, buttonLs, levelObjects, UI, MiniJoeSkillsUI;
     private float leftFirework, rightFirework;
     public float fireworkCooldownLeft, fireworkCooldownRight;
     public bool victory;
     public static bool goingLS;
+    private bool firstTime;
 
     // Start is called before the first frame update
     void Start()
     {
         victoryPanel.SetActive(false);
-
+        firstTime = true;
     }
 
     // Update is called once per frame
@@ -25,6 +26,15 @@ public class VictoryController : MonoBehaviour
     {
         if (victory == true)
         {
+            if (firstTime)
+            {
+                if (GameObject.Find("MiniJoe")) GameObject.Find("MiniJoe").SetActive(false);
+                firstTime = false;
+            }
+
+            UI.SetActive(false);
+            MiniJoeSkillsUI.SetActive(false);
+            Time.timeScale = 1.0f;
             EventSystem.current.SetSelectedGameObject(null);
             EventSystem.current.SetSelectedGameObject(buttonLs);
             player.SetActive(false);
@@ -33,9 +43,9 @@ public class VictoryController : MonoBehaviour
             victoryUI.SetActive(true);
             if (leftFirework <= 0)
             {
-                randomValor = new Vector2(
+                randomValor = new Vector3(
                     Random.Range(positionA.x, positionB.x),
-                    Random.Range(positionA.y, positionB.y)
+                    Random.Range(positionA.y, positionB.y), 1
             );
                 Instantiate(firework, randomValor, Quaternion.identity);
                 leftFirework = fireworkCooldownLeft;
@@ -44,12 +54,12 @@ public class VictoryController : MonoBehaviour
             {
                 leftFirework -= Time.deltaTime;
             }
-
+            
             if (rightFirework <= 0)
             {
-                randomValor2 = new Vector2(
+                randomValor2 = new Vector3(
                     Random.Range(positionC.x, positionD.x),
-                    Random.Range(positionC.y, positionD.y)
+                    Random.Range(positionC.y, positionD.y), 1
                  );
                 Instantiate(firework, randomValor2, Quaternion.identity);
                 rightFirework = fireworkCooldownRight;
