3df7583c5c3b97e28dd00c7f858bd83847a8ed8d
Josep <josep.romera@enti.cat>
2022-01-15 16:18:19 +0100

    Arreglado problema continueButton

--- Assets/Proyecto/Scripts/MainMenu/MainMenuController.cs
+++ Assets/Proyecto/Scripts/MainMenu/MainMenuController.cs
@@ -10,8 +10,6 @@ public class MainMenuController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
         if (VictoryController.goingLS == true || GameOver.goingLS == true)
         {
             VictoryController.goingLS = false;
