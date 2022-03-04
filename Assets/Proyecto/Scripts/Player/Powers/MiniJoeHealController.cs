a0e5453482e003dc454ecb604c6d2eb04fb62e42
Ivan <ivan.sales@enti.cat>
2022-03-04 17:04:20 +0100

    gitflow-feature-stash: PruebaCuracion

--- Assets/Proyecto/Scripts/Player/Powers/MiniJoeHealController.cs
+++ Assets/Proyecto/Scripts/Player/Powers/MiniJoeHealController.cs
@@ -16,9 +16,12 @@ public class MiniJoeHealController : MonoBehaviour
     public PlayerHealthController phc;
     private bool healedOnce;
     public GameObject healParticle;
+    public GameObject player;
+    private float area2;
     
     void Start()
     {
+        area2 = 6.7f;
         timer = healTimer;
         healedOnce = false;
         //timer2 = healDelay;
@@ -28,13 +31,13 @@ public class MiniJoeHealController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<MiniJoe>().displanted == false)
+        if (GetComponent<MiniJoe>().displanted == true && Vector2.Distance(this.transform.position, player.transform.position) > area2)
         {
             if (phc.currentHealth < phc.health && currenntHealsAvailable > 0) //&& !healOnce
             {
                 if (timer <= 0)
                 {
-                    var ps = Instantiate(healParticle, new Vector2(this.transform.position.x, this.transform.position.y), Quaternion.identity);
+                    var ps = Instantiate(healParticle, new Vector2(player.transform.position.x, player.transform.position.y), Quaternion.identity);
                     phc.currentHealth += healAmmount;
                     phc.currentHealth = Mathf.Round(phc.currentHealth * 10.0f) * 0.1f; //Resondear a unn decimal porque a veces no se suma bien
                     if (phc.currentHealth == 3.0f || phc.currentHealth == 2.0f || phc.currentHealth == 1.0f)
