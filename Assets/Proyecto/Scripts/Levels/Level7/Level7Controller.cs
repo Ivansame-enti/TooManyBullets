ae507ff04a7a7c5e02da198dd3778a1d01d100fd
Pablo <pperpinyanc@gmail.com>
2022-03-11 19:31:11 +0100

    fases 1-3 hechas

--- Assets/Proyecto/Scripts/Levels/Level7/Level7Controller.cs
+++ Assets/Proyecto/Scripts/Levels/Level7/Level7Controller.cs
@@ -10,9 +10,15 @@ public class Level7Controller : MonoBehaviour
     public GameObject phase3;
     public GameObject phase4;
     public GameObject scenario;
-    public GameObject phase21, phase22, phase23;
+    public GameObject phase21, phase22, phase23, phase24, phase25;
+    public GameObject multi2, multi4;
+    private GameObject multi2a, multi4a;
     private int phasecounter;
     private bool oneTime;
+    private float time = 0.0f, timeaux = 0.0f;
+    private bool cambio = true;
+    private bool existe = false;
+    private bool existe2 = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +26,8 @@ public class Level7Controller : MonoBehaviour
 
         // scenario.SetActive(true);
         //phase1.SetActive(true); 
-        //scenario.SetActive(true);
-        phase3.SetActive(true);
+        scenario.SetActive(true);
+        phase1.SetActive(true);
     }
 
     // Update is called once per frame
@@ -34,10 +40,52 @@ public class Level7Controller : MonoBehaviour
             scenario.SetActive(false);
         }
 
-        if (phase2 && phase21 == null &&phase22 == null && phase23==null)
+        if (phase2 && phase21 == null &&phase22 == null && phase23==null && phase24 ==null && phase25==null)
         {
+
             phase2.SetActive(false);
             phase3.SetActive(true);
+
+            if (!existe)
+            {
+                multi2a = Instantiate(multi2);
+                existe = true;
+            }
+            if (!existe2)
+            {
+                multi4a = Instantiate(multi4);
+                existe2 = true;
+            }
+
+
+            if (cambio){
+      
+                multi2a.SetActive(true);
+                time += Time.deltaTime;
+                if (time >= 5.0f)
+                {
+                    Destroy(multi2a);
+                    existe = false;
+                    cambio = false;
+                    time = 0.0f;
+                }
+            }
+            if (!cambio)
+            {
+             
+                multi4a.SetActive(true);
+                //multi4.SetActive(true);
+                time += Time.deltaTime;
+                if(time >= 5.0f)
+                {
+
+                    Destroy(multi4a);
+                    existe2 = false;
+                    cambio = true;
+                    time = 0.0f;
+                }
+            }
+
 
        
         }
