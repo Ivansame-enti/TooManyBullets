7725a44ceb6161e64766317c6b79f06aaa7e7ae8
Ivan <ivan.sales@enti.cat>
2022-03-18 19:07:32 +0100

    Acabadas particulas de enemigos especiales

--- Assets/SpecialEnemyParticles.cs
+++ Assets/SpecialEnemyParticles.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpecialEnemyParticles : MonoBehaviour
 {
     //public GameObject target;
+    public GameObject heatHeal;
     private float speed;
     private GameObject target;
     // Start is called before the first frame update
@@ -18,12 +19,12 @@ public class SpecialEnemyParticles : MonoBehaviour
     void Update()
     {
         this.transform.position = Vector2.MoveTowards(this.transform.position, target.transform.position, speed * Time.deltaTime);
-        Debug.Log(Vector3.Distance(this.transform.position, target.transform.position));
-        //Debug.Log("Eyyyyy");
+        //Debug.Log(Vector3.Distance(this.transform.position, target.transform.position));
         if (Vector3.Distance(this.transform.position, target.transform.position) == 90f)
         {
             
             GameObject.Find("MiniJoe").GetComponent<MiniJoeHealController>().currenntHealsAvailable++;
+            Instantiate(heatHeal, this.transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
