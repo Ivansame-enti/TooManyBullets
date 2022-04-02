7867f44534f51b8a9ae1230dd9977c3d27d44882
Ivan <ivan.sales@enti.cat>
2022-04-02 23:36:48 +0200

    Acabada de arreglar la música, falta SFX

--- Assets/SnapshotsController.cs
+++ Assets/SnapshotsController.cs
@@ -16,39 +16,45 @@ public class SnapshotsController : MonoBehaviour
     {
         bLow = false;
         bPause = false;
+        normalHealth.TransitionTo(0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!pc.pauseState)
+        if (phc != null && pc != null)
         {
-            if (phc.currentHealth < 2 && !bLow)
+            if (!pc.pauseState)
             {
-                lowHealth.TransitionTo(.5f);
-                bLow = true;
-            }
-            else if (phc.currentHealth == 3 && bLow)
-            {
-                normalHealth.TransitionTo(.01f);
-                bLow = false;
-            }
-            else if (phc.currentHealth >= 2 && bLow)
-            {
-                normalHealth.TransitionTo(.5f);
-                bLow = false;
-            }
-        }
+                if (phc.dead)
+                {
+                    normalHealth.TransitionTo(.5f);
+                    bLow = false;
+                }
 
-        if (pc.pauseState && !bPause)
-        {
-            pause.TransitionTo(0f);
-            bPause = true;
-        } else if(!pc.pauseState && bPause)
-        {
-            if(bLow) lowHealth.TransitionTo(.01f);
-            else normalHealth.TransitionTo(.01f);
-            bPause = false;
+                if (phc.currentHealth < 2 && !bLow)
+                {
+                    lowHealth.TransitionTo(.5f);
+                    bLow = true;
+                }
+                else if (phc.currentHealth >= 2 && bLow)
+                {
+                    normalHealth.TransitionTo(.5f);
+                    bLow = false;
+                }
+            }
+
+            if (pc.pauseState && !bPause)
+            {
+                pause.TransitionTo(0f);
+                bPause = true;
+            }
+            else if (!pc.pauseState && bPause)
+            {
+                if (bLow) lowHealth.TransitionTo(.01f);
+                else normalHealth.TransitionTo(.01f);
+                bPause = false;
+            }
         }
     }
 }
