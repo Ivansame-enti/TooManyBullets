3e8bb958fcbc8cf84d00e6ec6ad5d820200ecabc
Ivan <ivan.sales@enti.cat>
2022-04-02 20:54:18 +0200

    Pausa modifica la música

--- Assets/SnapshotsController.cs
+++ Assets/SnapshotsController.cs
@@ -8,23 +8,47 @@ public class SnapshotsController : MonoBehaviour
     public AudioMixerSnapshot normalHealth;
     public AudioMixerSnapshot lowHealth;
     public PlayerHealthController phc;
+    public PauseController pc;
+    public AudioMixerSnapshot pause;
+    private bool bLow, bPause;
     // Start is called before the first frame update
     void Start()
     {
+        bLow = false;
+        bPause = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(phc.currentHealth < 2)
+        if (!pc.pauseState)
         {
-            lowHealth.TransitionTo(.5f);
-        } else if(phc.currentHealth == 3)
+            if (phc.currentHealth < 2 && !bLow)
+            {
+                lowHealth.TransitionTo(.5f);
+                bLow = true;
+            }
+            else if (phc.currentHealth == 3 && bLow)
+            {
+                normalHealth.TransitionTo(.01f);
+                bLow = false;
+            }
+            else if (phc.currentHealth >= 2 && bLow)
+            {
+                normalHealth.TransitionTo(.5f);
+                bLow = false;
+            }
+        }
+
+        if (pc.pauseState && !bPause)
         {
-            normalHealth.TransitionTo(.01f);
-        } else
+            pause.TransitionTo(0f);
+            bPause = true;
+        } else if(!pc.pauseState && bPause)
         {
-            normalHealth.TransitionTo(.5f);
+            if(bLow) lowHealth.TransitionTo(.01f);
+            else normalHealth.TransitionTo(.01f);
+            bPause = false;
         }
     }
 }
