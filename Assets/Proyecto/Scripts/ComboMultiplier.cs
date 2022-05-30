3d019dcc54f16168c1811e2a3a51fce7baa794d4
Ivan <ivan.sales@enti.cat>
2022-05-30 17:47:31 +0200

    Se ha seguido con el canvas

--- Assets/Proyecto/Scripts/ComboMultiplier.cs
+++ Assets/Proyecto/Scripts/ComboMultiplier.cs
@@ -8,8 +8,10 @@ public class ComboMultiplier : MonoBehaviour
     public float maxTimer;
     private float timer;
     public TextMeshProUGUI multiplier;
+    public TextMeshProUGUI x;
     private int mult;
     private float scaleAmmount;
+    private float colorAmmount;
     float map(float s, float a1, float a2, float b1, float b2)
     {
         return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
@@ -26,14 +28,32 @@ public class ComboMultiplier : MonoBehaviour
     void Update()
     {
         //if(muere enemigo) this.transform.position = new Vector3(enemypos.x, enemypos.y +1.0f, enemypos.z);
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            mult++;
             multiplier.text = mult.ToString();
             timer = maxTimer;
         }
 
+        //if(mult > 5)
         scaleAmmount = map(timer, 0, maxTimer, 0, 2);
+        colorAmmount = map(timer, 0, maxTimer+2.0f, 0, 1);
+
         this.transform.localScale = new Vector3(scaleAmmount, scaleAmmount, scaleAmmount);
-        if(timer > 0) timer -= Time.deltaTime;
+        if (mult > 5 && mult < 10)
+        {
+            multiplier.color = new Color(1.0f, colorAmmount, 1.0f, 1.0f);
+            x.color = new Color(1.0f, colorAmmount, 1.0f, 1.0f);
+        }
+        else if (mult >= 10)
+        {
+            multiplier.color = new Color(1.0f, 1.0f, colorAmmount, 1.0f);
+            x.color = new Color(1.0f, 1.0f, colorAmmount, 1.0f);
+        } else
+        {
+            multiplier.color = new Color(1.0f, colorAmmount, colorAmmount, 1.0f);
+            x.color = new Color(1.0f, colorAmmount, colorAmmount, 1.0f);
+        }
+        if (timer > 0) timer -= Time.deltaTime;
     }
 }
